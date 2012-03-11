2b24182744d00df0539045cf1041dc0a083d00db
Chris Engstrom <tipher88@gmail.com>
2012-03-11 14:45:47 -0500

    Tweaked opening comment.

--- PE_Prob1/main.cpp
+++ PE_Prob1/main.cpp
@@ -1,6 +1,6 @@
-//****************************
-// Chris Engstrom			 *
-// Project Euler - Problem 1 *
+//*******************************
+// Chris Engstrom				*
+// Project Euler - Problem 1	*
 //**********************************************************
 // Find the sum of all the multiples of 3 or 5 below 1000. *
 //**********************************************************
